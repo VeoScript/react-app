fdb6425a7c8b5da5a95c6cb1e7d8e1a42df344d2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-22 17:36:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,33 @@ import faker from 'faker';
 
 function App() {
 
-  const [name, setName] = useState('Alan Smith')
+  const [name, setName] = useState('Jerome Villaruel')
   const [showCard, setShowCard] = useState(true)
+  const changeNameHandler = name => setName(name)
+  const changeInputHandler = event => setName(event.target.value)
+  const toggleShowCard = () => setShowCard(!showCard)
   const buttonsMarkup = (
     <div>
       <button className="button button2">YES</button>
       <button className="button button3">NO</button>
     </div>
   )
-  const changeNameHandler = name => setName(name)
-  const changeInputHandler = event => setName(event.target.value)
-  const toggleShowCard = () => setShowCard(!showCard)
-
+  const cardsMarkup = (
+    showCard &&
+    <Card
+      avatar='http://placeimg.com/640/480/people'
+      name={name}
+      title={faker.name.jobTitle()}
+      onChangedName={()=>changeNameHandler('Chelyn Hermosilla')}
+      onChangeInput={changeInputHandler}
+    >
+      {buttonsMarkup}
+    </Card>
+  )
   return (
    <div className="App">
-     <button className="button" onClick={toggleShowCard}>Toggle Show/Hide</button>
-     {showCard &&
-      <Card
-        avatar='http://placeimg.com/640/480/people'
-        name={name}
-        title={faker.name.jobTitle()}
-        onChangedName={()=>changeNameHandler('Jerome Villaruel')}
-        onChangeInput={changeInputHandler}
-      >
-        {buttonsMarkup}
-      </Card>
-    }
+    <button className="button" onClick={toggleShowCard}>Toggle Show/Hide</button>
+    {cardsMarkup}
    </div>
   );
 }
