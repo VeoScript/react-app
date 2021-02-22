10cfc16e4b4df8e37efca1ef9bba5fecedecd9b7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-22 17:24:52 +0800

    added toggleShowCard function

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import faker from 'faker';
 function App() {
 
   const [name, setName] = useState('Alan Smith')
-
+  const [showCard, setShowCard] = useState(true)
   const buttonsMarkup = (
     <div>
       <button className="button button2">YES</button>
       <button className="button button3">NO</button>
     </div>
   )
-
   const changeNameHandler = name => setName(name)
-
   const changeInputHandler = event => setName(event.target.value)
+  const toggleShowCard = () => setShowCard(!showCard)
 
   return (
    <div className="App">
-     <button className="button" onClick={()=>changeNameHandler('Chelyn Hermosilla')}>Toggle Show/Hide</button>
-     <Card
-      avatar='http://placeimg.com/640/480/people'
-      name={name}
-      title={faker.name.jobTitle()}
-      onChangedName={()=>changeNameHandler('Jerome Villaruel')}
-      onChangeInput={changeInputHandler}
-    >
-      {buttonsMarkup}
-    </Card>
+     <button className="button" onClick={toggleShowCard}>Toggle Show/Hide</button>
+     {showCard ?
+      <Card
+        avatar='http://placeimg.com/640/480/people'
+        name={name}
+        title={faker.name.jobTitle()}
+        onChangedName={()=>changeNameHandler('Jerome Villaruel')}
+        onChangeInput={changeInputHandler}
+      >
+        {buttonsMarkup}
+      </Card>:null}
    </div>
   );
 }
