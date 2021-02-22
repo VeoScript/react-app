b50a2c3ed1cd3ec9c990b4802a516dbee6b88a1c
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-22 17:11:31 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Card from './Card';
+import faker from 'faker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [name, setName] = useState('Alan Smith')
+
+  const buttonsMarkup = (
+    <div>
+      <button className="button button2">YES</button>
+      <button className="button button3">NO</button>
     </div>
+  )
+
+  const changeNameHandler = name => setName(name)
+
+  const changeInputHandler = event => setName(event.target.value)
+
+  return (
+   <div className="App">
+     <button className="button" onClick={()=>changeNameHandler('Chelyn Hermosilla')}>Change Name</button>
+     <Card
+      avatar='http://placeimg.com/640/480/people'
+      name={name}
+      title={faker.name.jobTitle()}
+      onChangedName={()=>changeNameHandler('Jerome Villaruel')}
+      onChangeInput={changeInputHandler}
+    >
+      {buttonsMarkup}
+    </Card>
+   </div>
   );
 }
 
