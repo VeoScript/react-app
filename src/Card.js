0b7b35f4da34e12ddbb05b350122994de8e62e7f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-26 11:59:26 +0800

    changed functional base to class base

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const Card = props => {
-  return (
-    <div className="container">
-      <div className="card">
-        <img src={props.avatar} alt="Avatar" style={{width: '100%'}} />
-        <div className="container">
-          <h4><b>{props.name}</b></h4> 
-          <p>{props.title}</p> 
-          <input type="text" onChange={props.onChangeName} value={props.name}/>
-          <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
-          <div>{props.children}</div> 
+class Card extends Component {
+  render() {
+    console.log('Card js rendering...')
+    return (
+      <div className="container">
+        <div className="card">
+          <img src={this.props.avatar} alt="Avatar" style={{width: '100%'}} />
+          <div className="container">
+            <h4><b>{this.props.name}</b></h4> 
+            <p>{this.props.title}</p> 
+            <input type="text" onChange={this.props.onChangeName} value={this.props.name}/>
+            <p><button className="button button-red" onClick={this.props.onDelete}>Delete</button></p>
+            <div>{this.props.children}</div> 
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Card
