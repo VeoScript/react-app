3ad614c12d35797451934c4d6ed8d661ea024280
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-26 11:59:37 +0800

    added title

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="app title" />
   </React.StrictMode>,
   document.getElementById('root')
 );
