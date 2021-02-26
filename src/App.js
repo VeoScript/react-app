4f1f54925615aa898d5edcdeede1a906c659fb2d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-02-26 10:23:19 +0800

    Convert functional base to class base

--- src/App.js
+++ src/App.js
@@ -1,101 +1,88 @@
-import React, { useState } from 'react';
-import { ThemeProvider, css } from 'styled-components';
+import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 import Card from './Card';
-import styled from 'styled-components';
+import Button from './Element/Button';
 
 const theme = {
   primary: '#4CAF50',
   mango: 'yellow'
 }
 
-//Custom design using ~styled-components
-const Button = styled.button`
-  ${props =>
-    props.color && css `
-      background-color: ${props=>props.length > 2 ? props.theme[props.color] : props.length < 2 ? 'red' : 'pink'};
-      color: ${props=>props.length <= 1 ? 'white' : 'black'};
-    `
-  }
-  border: none;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-`
-
-function App() {
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      name: 'Magpakailan Tamad',
-      title: 'Hindi Magbabago',
-      avatar:'http://placeimg.com/640/480/people'
-    },
-    {
-      id: 2,
-      name: 'Magpakailan Jakol',
-      title: 'Sige Rag Lusi',
-      avatar:'http://placeimg.com/640/480/people'
-    },
-    {
-      id: 3,
-      name: 'Magpakailan Tamod',
-      title: 'Pirme Pasirit',
-      avatar:'http://placeimg.com/640/480/people'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      cards: [
+        {
+          id: 1,
+          name: 'Magpakailan Tamad',
+          title: 'Hindi Magbabago',
+          avatar:'http://placeimg.com/640/480/people'
+        },
+        {
+          id: 2,
+          name: 'Magpakailan Jakol',
+          title: 'Sige Rag Lusi',
+          avatar:'http://placeimg.com/640/480/people'
+        },
+        {
+          id: 3,
+          name: 'Magpakailan Tamod',
+          title: 'Pirme Pasirit',
+          avatar:'http://placeimg.com/640/480/people'
+        }
+      ],
+      showCard: true
     }
-  ])
-  const [showCard, setShowCard] = useState(true)
-  const toggleShowCard = () => setShowCard(!showCard)
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards]
-    cards_copy.splice(cardIndex, 1)
-    setCards(cards_copy)
   }
-  const changedNameHandler = (event, id) => {
+  toggleShowCard = () => this.setState({showCard:!this.state.showCard})
+  deleteCardHandler = (cardIndex) => {
+    const cards_copy = [...this.state.cards]
+    cards_copy.splice(cardIndex, 1)
+    this.setState({cards:cards_copy})
+  }
+  changedNameHandler = (event, id) => {
     //1. Which Card
-    const cardIndex = cards.findIndex(card => card.id == id)
+    const cardIndex = this.state.cards.findIndex(card => card.id === id)
     //2. Make a copy of the Cards
-    const cards_copy = [...cards]
+    const cards_copy = [...this.state.cards]
     //3. Changed the name of the specific card
     cards_copy[cardIndex].name = event.target.value
     //4. Set the cards with the latest version of card copy
-    setCards(cards_copy)
+    this.setState({cards:cards_copy})
   }
   // const buttonStyle = {
   //   backgroundColor: null
   // }
+  render() {
+    const classes = ['button']
 
-  const classes = ['button']
+    if(this.state.cards.length < 3) classes.push('pink')
+    if(this.state.cards.length < 2) classes.push('red')
 
-  if(cards.length < 3) classes.push('pink')
-  if(cards.length < 2) classes.push('red')
-
-  const cardsMarkup = (
-    cards.map((card, index) =>
-      showCard &&
-      <Card
-        avatar={card.avatar}
-        name={card.name}
-        title={card.title}
-        key={card.id}
-        onDelete={()=>deleteCardHandler(index)}
-        onChangeName={(event)=>changedNameHandler(event, card.id)}
-      />
+    const cardsMarkup = this.state.showCard && (
+      this.state.cards.map((card, index) =>
+        <Card
+          avatar={card.avatar}
+          name={card.name}
+          title={card.title}
+          key={card.id}
+          onDelete={()=>this.deleteCardHandler(index)}
+          onChangeName={(event)=>this.changedNameHandler(event, card.id)}
+        />
+      )
     )
-  )
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Button color="mango" length={cards.length}>Toggle</Button>
-        <button className={classes.join(' ')} onClick={toggleShowCard}>Toggle Show/Hide</button>
-        {cardsMarkup}
-      </div>
-   </ThemeProvider>
-  );
+    return (
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Button color="mango" length={this.state.cards.length} onClick={this.toggleShowCard}>Toggle</Button>
+          <button className={classes.join(' ')} onClick={this.toggleShowCard}>Toggle Show/Hide</button>
+          {cardsMarkup}
+        </div>
+    </ThemeProvider>
+    );
+  }
 }
 
 export default App;
